8b695ab8ca011ae76c2a407dd0914b72dfd34d61
PRATIK VIRANI <viranipratik530@gmail.com>
2025-01-08 14:50:29 +0530

    value chengs

--- src/screen/FormScreen.tsx
+++ src/screen/FormScreen.tsx
@@ -14,7 +14,7 @@ import { ProfileClass } from "../components/ProfileClass";
 const countries = [
     { label: 'India', value: 'india' },
     { label: 'United State', value: 'united state' },
-    { label: 'Pakistan', value: 'pakistan' },
+    { label: 'Pakistan', value: 'russiya to india' },
     { label: 'Russiya', value: 'russiya' },
     { label: 'Malaysia', value: 'malaysia' },
     { label: 'Argentina', value: 'argentina' },
